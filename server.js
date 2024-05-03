c3a9ccfb45303d7073072a15ce12e6c482af93cf
lakansantos <rizesuoracle25@gmail.com>
2024-05-03 19:10:42 +0800

    Testing push

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("testing");
+import dotenv from 'dotenv';
+import express from 'express';
+
+async function server() {
+  // Load environment variables from .env file
+  dotenv.config();
+
+  const app = express();
+
+  const port = process.env.PORT || 3000; // Use port from environment variable or default to 3000
+
+  app.listen(port, () => {
+    console.log(`Listening to Port: ${port}`);
+  });
+}
+
+server();
